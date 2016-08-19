7cfb597eb25417ffcfdef5117087c5b84e3c8b93
Tatiana Korosteleva <t.korosteleva@rambler-co.ru>
2016-08-19 18:14:00 +0300

    #! typography;

--- js/_lang-selector.js
+++ js/_lang-selector.js
@@ -11,26 +11,20 @@
 
     activeLanguageHolder = languageSelector.find('li.active a');
     if (!activeLanguageHolder) {
-      console.log('can not detect lang - redirect to /ru');
       window.location.href = '/ru';
     }
 
     activeLanguage = activeLanguageHolder.attr('xml:lang');
 
     if (activeLanguage === 'ru' && !isRuAvailable) {
-      console.log('active lang ru, but not available - redirect to /ru');
       window.location.href = '/ru';
     }
     if (activeLanguage === 'en' && !isEngAvailable) {
-      console.log('active lang en, but not available - redirect to /eng');
       window.location.href = '/eng';
     }
 
     linkRu = isRuAvailable ? isRuAvailable.attr('href') : '/ru';
     linkEng = isEngAvailable ? isEngAvailable.attr('href') : '/eng';
-
-    console.log(linkRu);
-    console.log(linkEng);
 
     customLangBlock.find('a[data-lang="ru"]').attr('href', linkRu);
     customLangBlock.find('a[data-lang="en"]').attr('href', linkEng);
